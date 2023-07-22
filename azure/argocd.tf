bfd566d52449448a9b094b0ce115aaf94b1354d1
castleking1810 <ereldag18@gmail.com>
2023-07-22 17:54:37 +0300

    removed pointer to old data block

--- azure/argocd.tf
+++ azure/argocd.tf
@@ -3,7 +3,7 @@ module "argocd" {
 
   host = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
 
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
